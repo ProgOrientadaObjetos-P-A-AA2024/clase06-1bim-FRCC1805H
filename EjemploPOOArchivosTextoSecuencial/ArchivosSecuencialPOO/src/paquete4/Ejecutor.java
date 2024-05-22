8b02fcb780e7cc3736ac569a05a25d0b8b2d77dc
fabian <frcanar2@utpl.edu.ec>
2024-05-22 17:03:07 -0500

    clase06

--- EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete4/Ejecutor.java
+++ EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete4/Ejecutor.java
@@ -13,7 +13,20 @@ public class Ejecutor {
         String nombreArchivo = "empresas2.txt";
         
         Empresa c1 = new Empresa("Textiles del Ecuador", "Quito");
-        
+        ArchivoEscritura escritura = new ArchivoEscritura(nombreArchivo);
+        // establecer el valor del atributo registro
+        escritura.establecerRegistro(c1);
+        // estabalecer en el archivo el atributo del registro
+        escritura.establecerSalida();
+
+        // proceso para lectura del archivo
+        ArchivoLectura lectura = new ArchivoLectura(nombreArchivo);
+        lectura.establecerLista();
+        // System.out.println(lectura.toString());
+        System.out.println(lectura);
+        lectura.cerrarArchivo();
+
+    
         /*
         ArchivoEscritura escritura = new ArchivoEscritura(nombreArchivo);
         // establecer el valor del atributo registro
